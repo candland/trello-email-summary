aaa35d1476cbe7b5694ac99012745ebaf51a20e4
candland <candland@gmail.com>
2015-01-02 16:33:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trello Email Summary
+
+Create a summary of a Trello board formatted for HTML email. Emailing is left up to a mail command line application.
+
+    node . -k TRELLO-KEY -t USER-TOKEN -b BOARD-ID | mail
+
+[Trello API](https://trello.com/docs)
+
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/local/bin node
+
+var path = require("path");
+var pkg = require( path.join(__dirname, 'package.json') );
+var program = require('commander');
+var Q = require('q');
+var fs = require('fs');
+
+var TrelloBoard = require('./lib/TrelloBoard.js');
+var template = require('./lib/htmlTemplates.js');
+
+program
+    .version(pkg.version)
+    .option('-k, --key <key>', 'Trello API Key')
+    .option('-t, --token <token>', 'Trello API User Token')
+    .option('-b, --board <board>', 'Trello Board ID')
+    .parse(process.argv);
+
+//TrelloBoard(program.key, program.token, program.board).board()
+//.then(function(board) {
+	//console.log(JSON.stringify(board));
+//})
+Q.nfcall(fs.readFile, "board.json", "utf-8").then(function(json) {
+	return JSON.parse(json);
+})
+.then(template.appendRenderMemberFn)
+.then(template.appendFormatFns)
+.then(template.renderMail)
+.then(console.log)
+.catch(function(err) {
+	console.error("ERROR");
+	console.error(err);
+})
+.done();
+
