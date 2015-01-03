f90f8388065428e46b4ad84ce45c839493f0c18e
candland <candland@gmail.com>
2015-01-03 15:18:16 -0700

    Added lists & json options.

--- index.js
+++ index.js
@@ -9,28 +9,39 @@ var fs = require('fs');
 var TrelloBoard = require('./lib/trelloBoard.js');
 var template = require('./lib/htmlTemplates.js');
 
+function list(val) { return val.split(','); }
+
 program
     .version(pkg.version)
     .option('-k, --key <key>', 'Trello API Key')
     .option('-t, --token <token>', 'Trello API User Token')
     .option('-b, --board <board>', 'Trello Board ID')
+		.option('-l, --lists <listid[,listid]>', 'Include these lists', list)
+		.option('-j, --json', 'Output in JSON')
     .parse(process.argv);
 
-TrelloBoard(program.key, program.token, program.board).board()
-//.then(function(board) {  // Store file to tweak template
-	//console.log(JSON.stringify(board));
-//})
-//Q.nfcall(fs.readFile, "board.json", "utf-8").then(function(json) { // load stored file
+p = TrelloBoard(program.key, program.token, program.board, program.lists).board()
+//p = Q.nfcall(fs.readFile, "board.json", "utf-8").then(function(json) { // load stored file
 	//return JSON.parse(json);
 //})
-.then(template.appendRenderMemberFn)
-.then(template.appendLabelNameFn)
-.then(template.appendFormatFns)
-.then(template.renderMail)
-.then(console.log)
-.catch(function(err) {
-	console.error("ERROR");
-	console.error(err);
-})
-.done();
-
+if (program.json) {
+	p.then(function(board) {  // Store file to tweak template
+		console.log(JSON.stringify(board));
+	})
+	.catch(function(err) {
+		console.error("ERROR");
+		console.error(err);
+	})
+	.done();
+} else {
+	p.then(template.appendRenderMemberFn)
+	.then(template.appendLabelNameFn)
+	.then(template.appendFormatFns)
+	.then(template.renderMail)
+	.then(console.log)
+	.catch(function(err) {
+		console.error("ERROR");
+		console.error(err);
+	})
+	.done();
+}
